b18b3a99ab7a845351ebb1fb35516d3e8822f07f
Акиньшин Евгений <evgeny.akinshin@iva-tech.ru>
2022-04-10 12:29:26 +0600

    ChatRow has been to Chat group

--- SwiftUI Chat/ContentView.swift
+++ SwiftUI Chat/ContentView.swift
@@ -16,39 +16,7 @@ struct ContentView : View {
   }
 }
 
-struct ChatRow : View {
-  var chatMessage: ChatMessage
-  var body: some View {
-    Group {
-      if !chatMessage.isMe {
-        HStack {
-          Group {
-            Text(chatMessage.avatar)
-            Text(chatMessage.message)
-              .bold()
-              .padding(10)
-              .foregroundColor(Color.white)
-              .background(chatMessage.color)
-              .cornerRadius(10)
-          }
-        }
-      } else {
-        HStack {
-          Group {
-            Spacer()
-            Text(chatMessage.message)
-              .bold()
-              .foregroundColor(Color.white)
-              .padding(10)
-              .background(chatMessage.color)
-              .cornerRadius(10)
-            Text(chatMessage.avatar)
-          }
-        }
-      }
-    }
-  }
-}
+
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
