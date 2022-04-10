4c89afe2242fd143ac8c1775ea23c5517dfc7533
Акиньшин Евгений <evgeny.akinshin@iva-tech.ru>
2022-04-10 12:33:30 +0600

    code duplicates have been deleted from CHatRow

--- SwiftUI Chat/Chat/ChatRow.swift
+++ SwiftUI Chat/Chat/ChatRow.swift
@@ -3,32 +3,20 @@ import SwiftUI
 struct ChatRow : View {
   var chatMessage: ChatMessage
   var body: some View {
-    Group {
+    HStack {
       if !chatMessage.isMe {
-        HStack {
-          Group {
-            Text(chatMessage.avatar)
-            Text(chatMessage.message)
-              .bold()
-              .padding(10)
-              .foregroundColor(Color.white)
-              .background(chatMessage.color)
-              .cornerRadius(10)
-          }
-        }
+        Text(chatMessage.avatar)
       } else {
-        HStack {
-          Group {
-            Spacer()
-            Text(chatMessage.message)
-              .bold()
-              .foregroundColor(Color.white)
-              .padding(10)
-              .background(chatMessage.color)
-              .cornerRadius(10)
-            Text(chatMessage.avatar)
-          }
-        }
+        Spacer()
+      }
+      Text(chatMessage.message)
+        .bold()
+        .padding(10)
+        .foregroundColor(Color.white)
+        .background(chatMessage.color)
+        .cornerRadius(10)
+      if chatMessage.isMe {
+        Text(chatMessage.avatar)
       }
     }
   }
